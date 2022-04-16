98ee5e1c5ad9f66508053a82bcebb45526f37f28
ridhotamma <ridho.tamma@gmail.com>
2022-04-16 14:48:48 +0700

    initialize client projec

---
+++ pages/account.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Account() {
+  return (
+    <div>
+      <Head>
+        <title>Dashboard</title>
+        <meta name="description" content="Dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome to our Dashboard</h1>
+    </div>
+  );
+}
